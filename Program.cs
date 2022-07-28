ab2e9565d4e73ef82022fff69d23666319f4c94a
William Krug <william.p.krug@gmail.com>
2022-07-28 09:31:44 -0500

    removed extra blank line at end of files

--- Program.cs
+++ Program.cs
@@ -18,6 +18,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 
@@ -29,4 +33,3 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
-
